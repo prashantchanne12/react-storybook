12689a146cf3144787170054ca469d66ea7510d5
prashantchanne12 <prashantchanne121@gmail.com>
2021-03-13 21:31:19 +0530

    story within storry

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  // Sorting
   options: {
     storySort: (a, b) =>
       a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
---
+++ src/components/Subscription/Subscription.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Primary } from '../Button/Button.stories';
+import { Medium } from '../Input/Input.stories';
+
+export default {
+    title: 'form/Subscription'
+}
+
+export const PrimarySubscription = () => (
+    <>
+        <Medium />
+        <Primary />
+    </>
+)
